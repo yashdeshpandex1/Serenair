d543b16ec37639fd8a0bf585cfc7ba3c65b9a628
unknown <yashdeshpandex1@gmail.com>
2026-02-21 22:19:27 +0530

    feat: Streamlit climb phase detection page1

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-with open("./assets/images/flight_takeoff_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg", "r", encoding="utf-8") as f:
+with open("images/flight_takeoff_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg", "r", encoding="utf-8") as f:
     svg = f.read()
 
 st.markdown(
@@ -34,3 +34,23 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
+with st.container(border=True):
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image("./images/placeholder.png")
+    with col2:
+        st.markdown("""
+                    <div style="font-size:28px; font-weight:bold;">
+                        Climb Phase Detection
+                    </div> <br>
+                    """, unsafe_allow_html=True)
+        st.write("Detects whether the plane is:")
+        st.markdown("""
+                - Ascending
+                - Neutral
+                - Descending <br>
+                """, unsafe_allow_html=True)
+        if st.button("head over?"):
+            st.switch_page("./pages/page2.py")
+    
